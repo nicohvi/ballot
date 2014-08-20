5463aa1bbe6a5d14d1888a223e095297a402c41b
Nicolay Hvidsten <nico.hvi@gmail.com>
2014-08-20 13:52:07 +0200

    added has many to polls

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -2,6 +2,7 @@ class Poll < ActiveRecord::Base
   has_many :options, dependent: :destroy
   has_and_belongs_to_many :voters, class_name: 'User'
   belongs_to :owner, class_name: 'User', foreign_key: 'owner_id'
+  has_many :votes
 
   validates :name, presence: true,
                   length: { minimum: 5 }
