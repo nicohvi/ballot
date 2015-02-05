27dc62a23bb494e7da2c51008af71beb36792ec5
Nicolay Hvidsten <nico.hvi@gmail.com>
2015-02-05 13:42:23 +0100

    config error

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Ballot
   class Application < Rails::Application
   
     config.action_mailer.delivery_method = :postmark
-    config.action_mailer.postmark_settings = 'df7d7137-7e9a-4e52-8012-8089f58caafe'
+    config.action_mailer.postmark_settings = { api_token: 'df7d7137-7e9a-4e52-8012-8089f58caafe' }
 
   end
 end
