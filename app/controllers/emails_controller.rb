6f1c164d584b47c03ea6bd474aa8fd9dbec6fc59
Nicolay Hvidsten <nico.hvi@gmail.com>
2015-02-05 12:32:26 +0100

    skip verify CSRF for emails

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,4 +1,6 @@
 class EmailsController < ApplicationController
+  # No need for CSRF tokens for emails
+  skip_before_filter :verify_authenticity_token
 
   def handle
     @email = PostMark::Json.dece(request.body.read)
