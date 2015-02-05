442336bcb91989e3d8d2f8d58bd4dcce229b846b
Nicolay Hvidsten <nico.hvi@gmail.com>
2015-02-05 13:49:22 +0100

    host in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: 'ballot.cc' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -78,6 +79,8 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
