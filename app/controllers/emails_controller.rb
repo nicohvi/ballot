145a398e84b27cbb563e80ea3a372fd780a97ec0
Nicolay Hvidsten <nico.hvi@gmail.com>
2015-02-05 12:40:49 +0100

    skip verify CSRF for emails

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,10 +1,13 @@
+require 'postmark'
+
 class EmailsController < ApplicationController
   # No need for CSRF tokens for emails
   skip_before_filter :verify_authenticity_token
 
   def handle
-    @email = PostMark::Json.dece(request.body.read)
+    @email = PostMark::Json.decode(request.body.read)
     puts "hello heroku: {@email.inspect}"
+    render nothing: true, status: 200
   end
 
 end
